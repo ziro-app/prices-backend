a31217a2d4ee9cc84bdbadaea6917429e13f4d0c
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-05-06 21:32:12 -0700

    save to spreadsheet by updating each cell in a loop

--- src/editRow/editRowInSheet.js
+++ src/editRow/editRowInSheet.js
@@ -2,21 +2,29 @@ const auth = require('../authentication/auth')
 
 const editRowInSheet = (fabricante, data) => {
 	return new Promise( async (resolve, reject) => {
-		const { getRows } = await auth()
-		getRows(1, (error, rows) => {
+		const { getCells } = await auth()
+		getCells(1, (error, cells) => {
 			if (error)
-				reject({ message: 'Error in getRows', details: error })
-			const [ result ] = rows.filter(row => row.fabricante === fabricante)
-			console.log(result)
-			if (result) {
-				result.bijuteriamin = data.bijuteria_min
-				result.save(error => {
-					if (error)
-						reject({ message: 'Error in row.save', details: error })
-					resolve('ok')
+				reject({ message: 'Error in getCells', details: error })
+			console.log(cells)
+			const [ supplier ] = cells.filter(({ value }) => value === fabricante)
+			console.log(supplier)
+			if (supplier) {
+				const cellsToUpdate = cells.filter(({ row }) => row === supplier.row).slice(1)
+				const updatedCells = cellsToUpdate.map((cell, index) => {
+					cell.value = data[index + 2]
+					return cell
 				})
+				console.log(updatedCells)
+				for (let i = 0; i < updatedCells.length; i++) {
+					updatedCells[i].save(error => {
+						if (error)
+							reject({ message: 'Error in cell.Save', details: error })
+					})
+				}
+				resolve('ok')
 			} else
-				reject({ message: 'Error in server getRows', details: 'Row id does not exist' })
+				reject({ message: 'Error in getCells', details: 'Supplier does not exist' })
 		})
 	})
 }
